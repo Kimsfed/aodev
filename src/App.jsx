3e4bf2bb8333895d59e3dee13af5d2bd397d6c92
unknown <kimandre.aadland@gmail.com>
2023-04-24 20:54:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello verden</h1>
+        <p>Dette er en stor og fin dag syntes jeg</p>
+    </div>
+  )
+}
+
+export default App
