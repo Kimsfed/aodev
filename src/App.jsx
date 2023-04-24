4b2720418128e688ff1886438fa4fe29af065193
unknown <kimandre.aadland@gmail.com>
2023-04-24 22:01:54 +0200

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import "./App.scss";
+import "./SCSS/MainHeader.scss"
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello verden</h1>
-        <p>Dette er en stor og fin dag syntes jeg</p>
+    <div className="MainHeader">
+        <h1>Ao?</h1>
     </div>
   )
 }
